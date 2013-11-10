6399fb95e4c0706130ef2bb4db2d0926ac869ebc
Woo Hyeok Kang <wkang@mit.edu>
2013-11-09 22:59:28 -0500

    New modularized function.

--- colorDown.m
+++ colorDown.m
@@ -1,66 +1,57 @@
-function [colorReduced] = colorDown(rgbImage, varargin)
-%{
-  Input: rgbImage of dimension M by N, clusterTrue
-  8-bit RGB values (need verification on MATLAB)
-  Indeed image is stored as 8-bit for each color channel
-  
-  Output: colorReduced images
-  % Also 8-Bits
+function [reducedRGB]=colorDown(rgbImage, hNum, sNum, vNum)
+  hsvi = toHSV(rgbImage);
+  reducedHSV = reduceHSV(hsvi, hNum, sNum, vNum);
+  reducedRGB = toRGB(reducedHSV);
+end
 
-  Algorithm Note: In hardware we MUST implement a look-up table
-  going pixel-by-pixel for a decently sized image 625 by 808
-  took about 10 minutes
-  
-  The function pixelDown takes a long time!
-%}
-  
-  if (nargin < 2)
-    clusterTrue = false;
+% Wrapper so that we know which modules we need to work on
+function [hsvImage] = toHSV(rgbImage)
+  hsvImage = rgb2hsv(rgbImage);
+end
+
+function [rgbImage] = toRGB(hsvImage)
+  rgbImage = hsv2rgb(hsvImage);
+end
+
+function [map]=mapDivide(numBins)
+  map = linspace(1/numBins, 1, numBins);
+end
+
+function [reducedVal] = mapReduce(map, pixVal)
+  %Input: map specifies h/s/v, pixVal specifies h/s/v value at pixel
+  %Output: the thresholded value
+  %Suggestion: Binary Search Algorithm
+  numBin = size(map);
+  numBin = numBin(2);
+  if (pixVal <= map(1))
+    reducedVal = map(1);
   else
-    clusterTrue = true;
-  end
-
-  colorReduced=[];
-  sizeImg = size(rgbImage);
-  
-  rowN=sizeImg(1);
-  colN=sizeImg(2);
-
-  for i=1:rowN
-    for j=1:colN
-      disp(['i is this' num2str(i) 'j is this' num2str(j)]);
-      pixRGB = [rgbImage(i,j,1), rgbImage(i,j,2), rgbImage(i,j,3)];
-      newpixRGB=pixelDown(pixRGB);
-      for k=1:3
-	colorReduced(i,j,k) = newpixRGB(k);
+    for t=2:numBin
+      if (pixVal <= map(t))
+	reducedVal = map(t);
+	break;
       end
     end
   end
+  
 end
 
-function [newpxVal] = pixelDown(pixelRGB)
-%{
-  Input: pixelRGB is an array of length 3, total of 24-bit number, MSB first
-  RGB (we convert this using de2bi)
+function [reducedHSV] =reduceHSV(hsvImage, hNum, sNum, vNum)
+  hMap = mapDivide(hNum);
+  sMap = mapDivide(sNum);
+  vMap = mapDivide(vNum);
 
-  Output: newpxVal is also an array of length 3, total of 24-bit number
+  h=hsvImage(:,:,1); %Hue channel
+  s=hsvImage(:,:,2);
+  v=hsvImage(:,:,3);
+  
+  sz=size(h);
 
-  Algorithm: simple MSB(Most Significant Bit) Comparison
-%}
-  % Binary Wrapping, 8-Bit padding
-  bin_pixelR = de2bi(pixelRGB(1), 8, 'left-msb');
-  bin_pixelG = de2bi(pixelRGB(2), 8, 'left-msb');
-  bin_pixelB = de2bi(pixelRGB(3), 8, 'left-msb');
-  bin_pixelRGB = [bin_pixelR, bin_pixelG, bin_pixelB];
-
-  for i=0:5
-    if (bin_pixelRGB(i*4+1) ==1)
-      bin_newpxVal(i*4+1:(i+1)*4)=[1 1 1 1];
-    else
-      bin_newpxVal(i*4+1:(i+1)*4)=[0 0 0 0];
+  for pr=1:sz(1)
+    for pc=1:sz(2)
+      reducedHSV(pr,pc,1) = mapReduce(hMap, h(pr,pc));
+      reducedHSV(pr,pc,2) = mapReduce(sMap, s(pr,pc));
+      reducedHSV(pr,pc,3) = mapReduce(vMap, v(pr,pc));
     end
   end
-
-  % Decimal Wrapping
-  newpxVal = [bi2de(bin_newpxVal(1:8), 'left-msb'), bi2de(bin_newpxVal(9:16), 'left-msb'), bi2de(bin_newpxVal(17:24), 'left-msb')];
 end
--- edgeDetect.m
+++ edgeDetect.m
@@ -1,27 +1,42 @@
-A=imread('photo.JPG');
-B=rgb2gray(A);
-
-C=double(B);
-Thresh=40;
-
-for i=1:size(C,1)-2
-    for j=1:size(C,2)-2
-        %Sobel mask for x-direction:
-        Gx=((2*C(i+2,j+1)+C(i+2,j)+C(i+2,j+2))-(2*C(i,j+1)+C(i,j)+C(i,j+2)));
-        %Sobel mask for y-direction:
-        Gy=((2*C(i+1,j+2)+C(i,j+2)+C(i+2,j+2))-(2*C(i+1,j)+C(i,j)+C(i+2,j)));
-     
-        %The gradient of the image
-        B(i,j)=abs(Gx)+abs(Gy);
-        %B(i,j)=sqrt(Gx.^2+Gy.^2);
-     
-    end
+function [rgbEdge, edgeSel] = edgeDetect(rgbImage, threshold)
+  gsimg = rgb2gs(rgbImage);
+  [Gx, Gy] = gradCalc(gsimg);
+  G = absGrad(Gx, Gy);
+  [rgbEdge, edgeSel] = thresHold(G, threshold);
 end
-%Define a threshold value
 
-B=max(B,Thresh);
-B(B==round(Thresh))=0;
+% Wrapper so that we know which modules we need to work on
+function [gsImage] = rgb2gs(rgbImage)
+  gsImage = rgb2gray(rgbImage);
+  % This seems to be crucial in MATLAB implementation
+  gsImage = double(gsImage);
+end
 
-B=uint8(B);
-figure,imshow(~B);
+function [Gx, Gy] = gradCalc(gsImage)
+  sz = size(gsImage);
+  C = gsImage(:,:);
+  derivMask = [1,2,1];
+  
+  for i=1:sz(1)-2
+    for j=1:sz(2)-2
+	%Sobel mask for x-direction:
+      Gx(i,j)=((2*C(i+2,j+1)+C(i+2,j)+C(i+2,j+2))-(2*C(i,j+1)+C(i,j)+C(i,j+2)));
+        %Sobel mask for y-direction:
+      Gy(i,j)=((2*C(i+1,j+2)+C(i,j+2)+C(i+2,j+2))-(2*C(i+1,j)+C(i,j)+C(i+2,j)));
+    end
+  end
+end
 
+function [G] = absGrad(Gx, Gy)
+  G = abs(Gx) + abs(Gy);
+end
+
+function [rgbEdge, edgeSelect ] = thresHold(G, th)
+  % Input: th=threshold Value (8bit number)
+  edgeSelect = G > th;
+  edgeSelect = 1 - (1*edgeSelect);
+  rgbEdge = edgeSelect * 255;
+  %rgbEdge = max(G, th);
+  %rgbEdge(rgbEdge==round(th))=0;
+  %rgbEdge = uint8(rgbEdge);
+end
