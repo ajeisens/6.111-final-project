b2d571ff2bae708c52ed91eb4d619a1b5572b73c
Woo Hyeok Kang <wkang@mit.edu>
2016-04-20 21:55:50 -0400

    Added a dither script. Dithering done post-processing of all other modules

---
+++ scripts/s_dithered.m
@@ -0,0 +1,25 @@
+labRGB = imread('pictures/dark_knight.jpg');
+
+[edgRGB, edgSel] = edgeDetect(labRGB, 90);
+redRGB = colorDown(labRGB, 20, 10, 10);
+fusedRGB = fuseRGB(edgSel, edgRGB, redRGB);
+figure, imshow( fusedRGB );
+
+% source: http://www.mathworks.com/help/images/reducing-the-number-of-colors-in-an-image.html
+% tutorial on dithering
+% google search: colormap dither
+% numberColors = 16;
+% srcImg = labRGB;
+% [X_no_dither, map1] = rgb2ind( srcImg, numberColors, 'nodither' );
+% [X_dither, map2] = rgb2ind( srcImg, numberColors, 'dither' );
+
+%figure, imshow( X_dither,map2 );
+
+% source: dither matlab function
+
+bwMap =colorcube(8);
+        
+ditheredX = dither( fusedRGB, bwMap );
+% figure, imshow( ditheredX, bwMap );
+rgbConverted = ind2rgb( ditheredX, bwMap);
+figure, imshow( rgbConverted );
